7ea81bf19928583f70307194b884a46982e64d93
demoraesj <jpdemoraes@icloud.com>
2026-05-29 09:55:11 -0300

    Adiciona tratamento para credenciais inválidas

--- src/main/java/com/sistema/hospitalapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sistema/hospitalapi/exception/GlobalExceptionHandler.java
@@ -2,13 +2,17 @@ package com.sistema.hospitalapi.exception;
 
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -36,8 +40,23 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ApiErrorDTO handleGenericException(Exception ex){
+    public ApiErrorDTO handleGenericException(Exception ex) {
 
         return new ApiErrorDTO("Erro interno no servidor");
     }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Map<String, List<String>>> handleBadCredentials(
+            BadCredentialsException ex) {
+
+        Map<String, List<String>> body = new HashMap<>();
+
+        body.put("errors",
+                List.of("Email ou senha inválidos"));
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(body);
+    }
+
 }
